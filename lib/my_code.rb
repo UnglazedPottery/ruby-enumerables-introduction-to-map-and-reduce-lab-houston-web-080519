cc83c68d4a015e721d0b93a6cf359dcd70554ba1
Jade Huynh <35jadeh@gmail.com>
2019-07-22 22:02:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -55,4 +55,12 @@ def reduce_to_all_true(source_array)
 end
 
 def map_to_any_true(source_array)
+   i = 0 
+  while i < source_array.length do 
+    if (source_array[i] == false || source_array[i] == nil)
+      return false
+    end
+    i += 1
+  end
+  return true
 end
