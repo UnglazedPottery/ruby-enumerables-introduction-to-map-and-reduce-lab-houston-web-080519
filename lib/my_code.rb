9b4baaeb610b76e00223aaa7561ca2aae9db9bc4
Jade Huynh <35jadeh@gmail.com>
2019-07-22 21:51:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -45,7 +45,7 @@ end
 def reduce_to_all_true(source_array)
    i = 0 
   while i < source_array.length do 
-    if (source_array[i] == false || passengers_sounds[i] == "sneezing")
+    if (source_array[i] == false)
       return true
     end
     i += 1
