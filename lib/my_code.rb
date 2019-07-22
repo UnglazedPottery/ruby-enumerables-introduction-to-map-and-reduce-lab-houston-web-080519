77def453aee2823ae309f9f86d564a512d6a16f6
Jade Huynh <35jadeh@gmail.com>
2019-07-22 21:47:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -33,7 +33,13 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point)
-  
+   i = 0
+   total = 0
+  while i < source_array.length do 
+    total += total
+    i += 1
+  end
+  return false
 end
 
 def reduce_to_all_true(source_array)
